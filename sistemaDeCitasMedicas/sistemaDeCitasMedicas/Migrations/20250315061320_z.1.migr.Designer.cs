0063869692df3bae224eca058661bceb9244deb3
Zelidee <zelideeguemez@gmail.com>
2025-03-15 02:16:37 -0400

    z.1.commit.SoloCambios

--- sistemaDeCitasMedicas/sistemaDeCitasMedicas/Migrations/20250315061320_z.1.migr.Designer.cs
+++ sistemaDeCitasMedicas/sistemaDeCitasMedicas/Migrations/20250315061320_z.1.migr.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using sistemaDeCitasMedicas.Data;
 
@@ -10,9 +11,11 @@ using sistemaDeCitasMedicas.Data;
 namespace sistemaDeCitasMedicas.Migrations
 {
     [DbContext(typeof(CitaMedicaContext))]
-    partial class CitaMedicaContextModelSnapshot : ModelSnapshot
+    [Migration("20250315061320_z.1.migr")]
+    partial class z1migr
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.3");
---
+++ sistemaDeCitasMedicas/sistemaDeCitasMedicas/Migrations/20250315061320_z.1.migr.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sistemaDeCitasMedicas.Migrations
+{
+    /// <inheritdoc />
+    public partial class z1migr : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Medicos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false),
+                    Especialidad = table.Column<string>(type: "TEXT", nullable: false),
+                    HorarioDisponible = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Medicos");
+        }
+    }
+}
