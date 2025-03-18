1ffb2fafd0fb76b5aa3377fadeceb1e361faa612
Elias-D23 <Eliasdiazfeliz@gmail.com>
2025-03-18 17:24:30 -0400

    Ultimate Update

---
+++ sistemaDeCitasMedicas/sistemaDeCitasMedicas/Models/PacienteCitasViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace sistemaDeCitasMedicas.Models
+{
+    public class PacienteCitasViewModel
+    {
+        public Paciente Paciente { get; set; }
+        public List<Cita> Citas { get; set; }
+    }
+}
