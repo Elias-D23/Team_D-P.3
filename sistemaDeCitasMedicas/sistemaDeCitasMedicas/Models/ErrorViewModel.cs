df8c8d6ff3dcf135ee3b12fa99764c27d4bb0bdc
Elias-D23 <Eliasdiazfeliz@gmail.com>
2025-03-18 16:52:11 -0400

    Ultimate Update

---
+++ sistemaDeCitasMedicas/sistemaDeCitasMedicas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sistemaDeCitasMedicas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
